f8ebb135938d851d6207ff6d9ce85664bf1dd02b
cmcoder9 <cmcoder@gmail.com>
2021-01-16 14:48:11 -0800

    update

--- Missions_to_Mars/scrape_NASA_Mars_News.py
+++ Missions_to_Mars/scrape_NASA_Mars_News.py
@@ -9,8 +9,10 @@ def init_browser():
 
 
 def scrape():
+    # {} means creating a dictionary
     browser = init_browser()
-    listings = {}
+    news={} # {'key': 'value', 'key2': 'value'}; news_title'[key'] return 'value'
+    # news_p={}
 
     # Visit NASA Mars News Site
     url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
@@ -20,11 +22,13 @@ def scrape():
     html = browser.html
     soup = BeautifulSoup(html, "html.parser")
 
+    # {'title': 'THIS IS THE TITLE', 'paragraph': 'THIS IS THE PARAGRAPH'}
+    news_title=soup.find("a", class_="result-title").get_text()
+    news_p=soup.find("span", class_="result-paragraph-text").get_text()
+    news['title']=news_title
+    # news will look like {'title': 'THIS IS THE TITLE'}
+    news["paragraph"]=news_p
+    # news will look like {'title': 'THIS IS THE TITLE', 'paragraph': 'THIS IS THE PARAGRAPH'}
     
-    news_tile[""] = soup.find("a", class_="result-title").get_text()
-    news_p[""] = soup.find("span", class_="result-paragraph-text").get_text()
-    
-
-    return news_title
-    return news_p
+    return news
 
