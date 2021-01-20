1e1520800c7e5c0b4bf2d21302110986394bb687
cmcoder9 <cmcoder@gmail.com>
2021-01-19 22:00:26 -0800

    flask app done

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, jsonify
 from flask_pymongo import PyMongo
-import scrape_costa
+import scrape_NASA_Mars_News
+
+# import scrape_costa
 
 # Create an instance of Flask
 app = Flask(__name__)
@@ -11,14 +13,11 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
-def home():
+def index():
 
-    # Find one record of data from the mongo database
-    mars_data = mongo.db.collection.find_one()
-
-    # Return template and data
-    return render_template("index.html", )
-
+    # Find record of data from the mongo database
+    mars = mongo.db.mars_data.find_one()
+    return render_template("index.html", mars_html= mars)
 
 # Route that will trigger the scrape function
 @app.route("/scrape")
@@ -31,7 +30,7 @@ def scrape():
     mongo.db.collection.update({}, mars_data, upsert=True)
 
     # Redirect back to home page
-    return redirect("/")
+    return redirect("/", code=302)
 
 
 if __name__ == "__main__":
--- Missions_to_Mars/scrape_NASA_Mars_News.py
+++ Missions_to_Mars/scrape_NASA_Mars_News.py
@@ -1,34 +1,3 @@
 from splinter import Browser
-from bs4 import BeautifulSoup
-
-
-def init_browser():
-    # @NOTE: Replace the path with your actual path to the chromedriver
-    executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
-    return Browser("chrome", **executable_path, headless=False)
-
-
-def scrape():
-    # {} means creating a dictionary
-    browser = init_browser()
-    news={} # {'key': 'value', 'key2': 'value'}; news_title'[key'] return 'value'
-    # news_p={}
-
-    # Visit NASA Mars News Site
-    url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
-    browser.visit(url)
-
-    # Scraoe page into Soup
-    html = browser.html
-    soup = BeautifulSoup(html, "html.parser")
-
-    # {'title': 'THIS IS THE TITLE', 'paragraph': 'THIS IS THE PARAGRAPH'}
-    news_title=soup.find("a", class_="result-title").get_text()
-    news_p=soup.find("span", class_="result-paragraph-text").get_text()
-    news['title']=news_title
-    # news will look like {'title': 'THIS IS THE TITLE'}
-    news["paragraph"]=news_p
-    # news will look like {'title': 'THIS IS THE TITLE', 'paragraph': 'THIS IS THE PARAGRAPH'}
-    
-    return news
-
+from bs4 import BeautifulSoup as bs
+from webdriver_manager.chrome import ChromeDriverManager
